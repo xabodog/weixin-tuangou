764641b089b79439dfbcbb2e0d9a9858615bf082
zhangzhicong <623688577@qq.com>
2016-05-18 09:29:48 +0800

    Initial Commit

---
+++ 微信/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  微信
+//
+//  Created by qingyun on 16/5/18.
+//  Copyright © 2016年 zzc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
